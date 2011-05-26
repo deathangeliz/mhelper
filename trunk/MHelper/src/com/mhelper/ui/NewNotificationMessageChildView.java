223cbcd46f74862e056e81bf578b07af7835ca20
deathangeliz.blue@gmail.com <deathangeliz.blue@gmail.com@8253430f-eb6e-5dcc-216f-baa9edcbb71b>
2011-05-26 01:56:11 +0000

    ui小更新

---
+++ trunk/MHelper/src/com/mhelper/ui/NewNotificationMessageChildView.java
@@ -0,0 +1,27 @@
+package com.mhelper.ui;
+
+import com.mhelper.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class NewNotificationMessageChildView extends LinearLayout {
+	EditText newNotificationEdit;
+	
+	public NewNotificationMessageChildView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		String infService = Context.LAYOUT_INFLATER_SERVICE;
+		LayoutInflater li;
+		li = (LayoutInflater)getContext().getSystemService(infService);
+		li.inflate(R.layout.new_notification_message, this, true);
+		
+		newNotificationEdit = (EditText)findViewById(R.id.newNotificationEdit);
+	}
+
+	public String getEditText() {
+		return newNotificationEdit.getText().toString();
+	}
+}
---
+++ trunk/MHelper/src/com/mhelper/ui/NewNotificationSelectChildView.java
@@ -0,0 +1,55 @@
+package com.mhelper.ui;
+
+import com.mhelper.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class NewNotificationSelectChildView extends LinearLayout {
+	TextView newNotificationText;
+	Spinner newNotificationSpinner;
+	int selectedType = 0;
+
+	public NewNotificationSelectChildView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		String infService = Context.LAYOUT_INFLATER_SERVICE;
+		LayoutInflater li;
+		li = (LayoutInflater)getContext().getSystemService(infService);
+		li.inflate(R.layout.new_notification_selected_child, this, true);
+		
+		newNotificationText = (TextView)findViewById(R.id.newNotificationText);
+		newNotificationSpinner = (Spinner)findViewById(R.id.newNotificationSpinner);
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                context, R.array.message_type, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		newNotificationSpinner.setAdapter(adapter);
+		newNotificationSpinner.setOnItemSelectedListener(
+                new OnItemSelectedListener() {
+                    public void onItemSelected(
+                            AdapterView<?> parent, View view, int position, long id) {
+                    	selectedType = position;
+                    }
+
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    	selectedType = 0;
+                    }
+                });
+	}
+	
+	public void setNotificationSelectText(String str){
+		newNotificationText.setText(str);
+	}
+
+	public int getSelectedType(){
+		return selectedType;
+	}
+}
