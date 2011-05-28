d213331f8e799d84fb2fd6876951cd187aa47178
peterchou139@gmail.com <peterchou139@gmail.com@8253430f-eb6e-5dcc-216f-baa9edcbb71b>
2011-05-28 13:32:56 +0000

    修改了 NewAlarmChildView.java 和 new_alarm_child.xml

--- trunk/MHelper/src/com/mhelper/middle/TsdBroadcastReceiver.java
+++ trunk/MHelper/src/com/mhelper/middle/TsdBroadcastReceiver.java
@@ -1,5 +1,5 @@
 package com.mhelper.middle;
 
 public class TsdBroadcastReceiver {
-
+	
 }
--- trunk/MHelper/src/com/mhelper/ui/NewAlarmChildView.java
+++ trunk/MHelper/src/com/mhelper/ui/NewAlarmChildView.java
@@ -2,13 +2,17 @@ package com.mhelper.ui;
 
 import com.mhelper.R;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,50 +22,82 @@ import android.widget.TimePicker;
 import java.util.Calendar;
 
 public class NewAlarmChildView extends LinearLayout {
-	Context ctx;
-	
-	TextView newAlarmDate;
-	EditText newAlarmTime;
+	private Context context;
+	private AlertDialog.Builder ad;
+	private DatePickerDialog dpd;
+	private TimePickerDialog tpd;
+	TextView alarmDate;
+	TextView alarmTime;
 	ImageView newAlarmImage;
+	int position = 0;
 	
-    static final int TIME_DIALOG_ID = 0;
-    static final int DATE_DIALOG_ID = 1;
-	
-	public NewAlarmChildView(Context context) {
-		super(context);
-		ctx = context;
+	public NewAlarmChildView(Context _context, int position){
+		super(_context);
+		this.context = _context;
+		this.position = position;
 		// TODO Auto-generated constructor stub
 		String infService = Context.LAYOUT_INFLATER_SERVICE;
 		LayoutInflater li;
 		li = (LayoutInflater)getContext().getSystemService(infService);
 		li.inflate(R.layout.new_alarm_child, this, true);
 		
-		newAlarmDate = (EditText)findViewById(R.id.newAlarmDate);
-		newAlarmTime = (EditText)findViewById(R.id.newAlarmTime);
+		alarmDate = (TextView)findViewById(R.id.dateButton);
+		alarmTime = (TextView)findViewById(R.id.timeButton);
 		newAlarmImage = (ImageView)findViewById(R.id.newAlarmImage);
 		
-		newAlarmDate.setOnClickListener(new OnClickListener() {
+		alarmDate.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				OnDateSetListener listener = new OnDateSetListener() {
+					
+					@Override
+					public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+						NewCondSettings newCondSettings = (NewCondSettings)context;
+						newCondSettings.condAlarmDay = arg1;
+						newCondSettings.condAlarmMonth = arg2;
+						newCondSettings.condAlarmDay = arg3;
+						String dateStr = arg1 + "/" + (arg2+1) + "/" + arg3;
+						setAlarmDate(dateStr);
+					}
+				};
+				Calendar nowTime = Calendar.getInstance();
+				dpd = new DatePickerDialog(context, listener, nowTime.get(Calendar.YEAR), nowTime.get(Calendar.MONTH), nowTime.get(Calendar.DAY_OF_MONTH));
+				dpd.show();
 			}
 		});
-		newAlarmTime.setOnClickListener(new OnClickListener() {
+		alarmTime.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				OnTimeSetListener listener = new OnTimeSetListener() {
+					
+					@Override
+					public void onTimeSet(TimePicker arg0, int arg1, int arg2) {
+						NewCondSettings newCondSettings = (NewCondSettings)context;
+						newCondSettings.condAlarmHour = arg1;
+						newCondSettings.condAlramMitute = arg2;
+						String timeStr = arg1 + ":";
+						if(arg2 < 10){
+							timeStr = timeStr + "0" + arg2;
+						}else
+							timeStr = timeStr + arg2;
+						setAlarmTime(timeStr);
+					}
+				};
+				Calendar nowTime = Calendar.getInstance();
+				tpd = new TimePickerDialog(context, listener, nowTime.get(Calendar.HOUR_OF_DAY), nowTime.get(Calendar.MINUTE), false);
+				tpd.show();
 			}
 		});
 	}
 
 	public void setAlarmDate(String str){
-		newAlarmDate.setText(str);
+		alarmDate.setText(str);
 	}
 	
 	public void setAlarmTime(String str) {
-		newAlarmTime.setText(str);
+		alarmTime.setText(str);
 	}
 	
 	public void setAlarmImage(Drawable drawable) {
